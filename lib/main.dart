157e321f4ea4144c6bd6f847294def385eb99fb3
itika-j <itikajain12@gmail.com>
2023-06-02 12:44:12 +0530

    material widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
+
   runApp(
-      Center(
-          child:Text(
-              "Hello flutte",
-              textDirection: TextDirection.ltr
-          )
+      MaterialApp(
+        title: "My App",
+        home: Material(
+          color: Colors.amber,
+          child: Center(
+              child:Text(
+                  "Hello flutter",
+                  textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.blueAccent,fontSize: 40.0),
+              )
+          ),
+        ),
       )
   );
 }
